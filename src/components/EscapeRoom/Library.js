67191cea6d0a0afc41c816ae1fcd5d5500591c60
Rachel Lin <rlin@Rachels-MacBook-Pro.local>
2021-05-18 11:48:51 +0800

    add library books

--- src/components/EscapeRoom/Library.js
+++ src/components/EscapeRoom/Library.js
@@ -28,7 +28,18 @@ const hallway1 = {
   overflow: "hidden",
 }
 
-const book1 = {
+const bookOrange = {
+  left: "12.67%",
+  top: "45.38%",
+  width: "1.67%",
+  height: "4.38%",
+  position: "absolute",
+  display: "block",
+  zIndex: "5",
+  overflow: "hidden",
+}
+
+const bookRed = {
   left: "20.33%",
   top: "63.25%",
   width: "1.67%",
@@ -39,12 +50,23 @@ const book1 = {
   overflow: "hidden",
 }
 
-const book2 = {
+const bookYellow = {
   position: "absolute",
   left: "14.25%",
   top: "54.63%",
   width: "1.67%",
   height: "4.25%",
+  display: "block",
+  zIndex: "5",
+  overflow: "hidden",
+}
+
+const bookBlue = {
+  position: "absolute",
+  left: "15.83%",
+  top: "71.63%",
+  width: "1.67%",
+  height: "4.38%",
   display: "block",
   zIndex: "5",
   overflow: "hidden",
@@ -94,15 +116,35 @@ export default class Library extends Component {
         <img src={S3Url + "/er/Library.png"} width="100%" />
         <Link to="/er/hallway1" style={hallway1} />
 
-        <ZoomModal style={book1}>
+        <ZoomModal style={bookOrange}>
           <img
-            src={S3Url + "/er/boba_book.png"}
-            alt="book cover"
+            src={S3Url + "/er/book_canals.png"}
+            alt="book canals"
             style={{ width: "500px", height: "600px" }}
           />
         </ZoomModal>
-
-        <div style={book2} onClick={this.handleBookClick} />
+        <ZoomModal style={bookRed}>
+          <img
+            src={S3Url + "/er/boba_book.png"}
+            alt="book boba"
+            style={{ width: "500px", height: "600px" }}
+          />
+        </ZoomModal>
+        <ZoomModal style={bookYellow}>
+          <img
+            src={S3Url + "/er/book_egypt.png"}
+            alt="book egypt"
+            style={{ width: "500px", height: "600px" }}
+            onClick={this.handleBookClick}
+          />
+        </ZoomModal>
+        <ZoomModal style={bookBlue}>
+          <img
+            src={S3Url + "/er/book_japan.png"}
+            alt="book japan"
+            style={{ width: "500px", height: "600px" }}
+          />
+        </ZoomModal>
 
         <ZoomModal style={magazine1}>
           <img
