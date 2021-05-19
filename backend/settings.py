8b822bc521e8a750b14fa694dcee5f37e7d1d72f
Rachel Lin <rlin@Rachels-MacBook-Pro.local>
2021-05-19 16:10:41 +0800

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -69,6 +69,10 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':
+    ('rest_framework.authentication.TokenAuthentication', )
+}
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
